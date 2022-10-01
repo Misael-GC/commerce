fd0c9fdb1297e58f40498e7f188e67287512db64
Misael-GC <misael.gomez1623@gmail.com>
2022-09-30 20:51:53 -0500

    useEffect y consumiendo una API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import ProductItem from '../components/ProductItem';
-import '../styles/ProductList.scss';
+import React, { useEffect, useState } from 'react';
+import ProductItem from '@components/ProductItem';
+import '@styles/ProductList.scss';
+import axios from 'axios';
+
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+	const [products, setProducts] = useState([]);//el estado inicial es [] porque la api asi esta estructurada
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const response = await axios(API);
+			setProducts(response.data);
+		}
+		fetchProducts();
+	},[]);
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product =>(
+					<ProductItem />
+				))}
 			</div>
 		</section>
 	);
